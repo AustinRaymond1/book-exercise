8284607026e58c5026095b7d13b92eaa99ee8538
Austin Raymond <43154662+AustinRaymond1@users.noreply.github.com>
2018-09-17 21:09:14 -0400

    Homework

--- Book.java
+++ Book.java
@@ -11,16 +11,66 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int borrow;
+    public boolean text;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int pages_number, boolean courseText)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = pages_number;
+        refNumber = "";
+        text = courseText;
     }
 
     // Add the methods here ...
+    public String getAuthor(){
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void printAuthor(){
+        System.out.println(author);
+    }
+    public void printTitle(){
+        System.out.println(title);
+    }
+    public int getPages(){
+        return pages;
+    }
+    public void setRefNumber(String ref){
+        if (ref.length() >= 3){
+            refNumber = ref;
+        }
+        else{
+            System.out.println("Error: Reference number not long enough");
+        }
+    }
+    public String getRefNumber(){
+        return refNumber;
+    }
+    public void printDetails(){
+        if (refNumber.length() == 0){ 
+            System.out.println("Title: " + title +". Author: " + author + ". Pages : " + pages + ". Referemce Number: ZZZ" + "Times borrowed: " + borrow + "Course Text: " + text);
+        }
+        else{
+            System.out.println("Title: " + title +". Author: " + author + ". Pages : " + pages + ". Referemce Number: " + refNumber + " Times borrowed: " + borrow + " Course Text: " + text);
+        }
+    }
+    public void borrowed(){
+        borrow += 1;
+    }
+    public int getBorrowed(){
+        return borrow;
+    }
+    public boolean isCourseText(){
+        return text;
+    }
 }
